0549bcd802c3bb1492f7e49e42edb26d7d48f3a9
ManiTej31 <manitejaaithu@gmail.com>
2022-12-03 13:21:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function toDark(){
+    document.body.style.backgroundColor="#242B2E";
+    document.getElementById("p").style.color = "#ffFF";
+}
+
+function toLight(){
+    document.body.style.backgroundColor="#ffffff";
+    document.getElementById("p").style.color = "#000000";
+}
